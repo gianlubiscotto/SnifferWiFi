7256a97b920e5ba65769de7c79c5c7e41f5485cb
Gianluca Ceccoli <ceccoli.gianluca@gmail.com>
2019-08-29 17:12:51 +0200

    Update gestione_dativ3-5.php

--- Website/php/gestione_dativ3-5.php
+++ Website/php/gestione_dativ3-5.php
@@ -3,17 +3,18 @@
 $macaddr = htmlspecialchars($_GET["macaddr"]);	
 $delta_reali = htmlspecialchars($_GET["delta_reali"]); 
 $delta_random = htmlspecialchars($_GET["delta_random"]); 
-$now_reali = htmlspecialchars($_GET["now_reali"]); 
-$now_random = htmlspecialchars($_GET["now_random"]);
+//$now_reali = htmlspecialchars($_GET["now_reali"]); 
+//$now_random = htmlspecialchars($_GET["now_random"]);
+$aggregates = htmlspecialchars($_GET["aggregates"]);
 $time = htmlspecialchars($_GET["time"]);	
+
 $macaddr = explode(",",$macaddr);
 $delta_reali = explode(",",$delta_reali);
 $delta_random = explode(",",$delta_random);
 $now_reali = explode(",",$now_reali);
 $now_random = explode(",",$now_random);
 $time = explode(",",$time);
-
-echo "delta_random array= ".$delta_random."<br>";
+$aggregates = explode(",",$aggregates);
 
 $id = htmlspecialchars($_GET["Id"]);
 
@@ -26,13 +27,14 @@ if (mysqli_connect_errno($conn)) {
 	echo "Failed to connect to database"; 
 }
 for($i=0;$i<count($macaddr);$i++){
-	$nr=(int)$now_reali[$i];
-	$nran=(int)$now_random[$i];
+	$aggr=(int)$aggregates[$i];
+	#$nr=(int)$now_reali[$i];
+	#$nran=(int)$now_random[$i];
 	$t=$time[$i];
 	$dr=(int)$delta_reali[$i];
 	$dran=(int)$delta_random[$i];
 	$m=$macaddr[$i];
-	$result = mysqli_query($conn, "INSERT INTO `logv3`(`Id`, `Mac_reali`, `Mac_random` , `Timestamp` , `delta_reali` , `delta_random` , `macaddr`) VALUES ('".$id."','".$nr."','".$nran."','".$t."','".$dr."','".$dran."','".$m."')") or die(mysqli_error($conn));
+	$result = mysqli_query($conn, "INSERT INTO `logv3`(`Id`, `aggregated` , `Timestamp` , `delta_reali` , `delta_random` , `macaddr`) VALUES ('".$id."','".$aggr."','".$t."','".$dr."','".$dran."','".$m."')") or die(mysqli_error($conn));
 	
 	echo mysqli_affected_rows($conn); 
 }
